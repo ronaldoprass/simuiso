7f77b14c6b30f93ecaa2bbe540f84ddde2252edc
Ronaldo Prass <rprass@gmail.com>
2020-12-03 10:34:12 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/cpsti/simuiso/SimuisoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cpsti.simuiso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuisoApplication.class, args);
+	}
+
+}
